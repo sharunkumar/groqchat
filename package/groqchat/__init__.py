97c4e4c3c6a4a3085bbf923d795a29f9f9a00bc3
Sharun <sharun@sharunkumar.com>
2025-03-05 23:41:28 -0800

    refactor: dynamic import of sounddevice and soundfile

--- package/groqchat/__init__.py
+++ package/groqchat/__init__.py
@@ -4,7 +4,6 @@ from prompt_toolkit import print_formatted_text, HTML
 from pathlib import Path
 import pprint, os, shutil, textwrap, wcwidth, requests, sys, subprocess, re, platform, psutil
 import speech_recognition as sr
-import sounddevice, soundfile
 
 thisFile = os.path.realpath(__file__)
 config.packageFolder = os.path.dirname(thisFile)
@@ -23,31 +22,39 @@ Path(userFolder).mkdir(parents=True, exist_ok=True)
 userFolder = os.path.join(config.localStorage, "gchat", "LLMs", "piper")
 Path(userFolder).mkdir(parents=True, exist_ok=True)
 
+
 def checkPyaudio():
     try:
         import sounddevice
         import speech_recognition as sr
-        mic = sr.Microphone() 
+
+        mic = sr.Microphone()
         del mic
         config.pyaudioInstalled = True
     except:
         if config.isTermux:
             config.pyaudioInstalled = False
-            #print2("Installing 'portaudio' and 'Pyaudio' ...")
-            #os.system("pkg install portaudio")
-            #config.pyaudioInstalled = True if installPipPackage("--upgrade PyAudio") else False
+            # print2("Installing 'portaudio' and 'Pyaudio' ...")
+            # os.system("pkg install portaudio")
+            # config.pyaudioInstalled = True if installPipPackage("--upgrade PyAudio") else False
         elif isCommandInstalled("brew"):
             print2("Installing 'portaudio' and 'Pyaudio' ...")
             os.system("brew install portaudio")
-            config.pyaudioInstalled = True if installPipPackage("--upgrade PyAudio") else False
+            config.pyaudioInstalled = (
+                True if installPipPackage("--upgrade PyAudio") else False
+            )
         elif isCommandInstalled("apt"):
             print2("Installing 'portaudio19-dev' and 'Pyaudio' ...")
             os.system("sudo apt update && sudo apt install portaudio19-dev")
-            config.pyaudioInstalled = True if installPipPackage("--upgrade PyAudio") else False
+            config.pyaudioInstalled = (
+                True if installPipPackage("--upgrade PyAudio") else False
+            )
         elif isCommandInstalled("dnf"):
             print2("Installing 'portaudio-devel' and 'Pyaudio' ...")
             os.system("sudo dnf update && sudo dnf install portaudio-devel")
-            config.pyaudioInstalled = True if installPipPackage("--upgrade PyAudio") else False
+            config.pyaudioInstalled = (
+                True if installPipPackage("--upgrade PyAudio") else False
+            )
         else:
             config.pyaudioInstalled = False
 
@@ -55,9 +62,13 @@ def checkPyaudio():
         print3("Note: 'pyAudio' is not installed.")
         print1("It is essential for built-in voice recognition feature.")
 
+
 def playAudio(audioFile):
-    sounddevice.play(*soundfile.read(audioFile)) 
+    import sounddevice, soundfile
+
+    sounddevice.play(*soundfile.read(audioFile))
     sounddevice.wait()
+
 
 def voiceTyping():
     # reference: https://github.com/Uberi/speech_recognition/blob/master/examples/microphone_recognition.py
@@ -65,14 +76,16 @@ def voiceTyping():
     r = sr.Recognizer()
     with sr.Microphone() as source:
         if config.voiceTypingNotification:
-            playAudio(os.path.join(config.packageFolder, "audio", "notification1_mild.mp3"))
-        #run_in_terminal(lambda: print2("Listensing to your voice ..."))
+            playAudio(
+                os.path.join(config.packageFolder, "audio", "notification1_mild.mp3")
+            )
+        # run_in_terminal(lambda: print2("Listensing to your voice ..."))
         if config.voiceTypingAdjustAmbientNoise:
             r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
     if config.voiceTypingNotification:
         playAudio(os.path.join(config.packageFolder, "audio", "notification2_mild.mp3"))
-    #run_in_terminal(lambda: print2("Processing to your voice ..."))
+    # run_in_terminal(lambda: print2("Processing to your voice ..."))
     if config.voiceTypingPlatform == "google":
         # recognize speech using Google Speech Recognition
         try:
@@ -81,18 +94,26 @@ def voiceTyping():
             # config.voiceTypingLanguage should be code list in column BCP-47 at https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages
             return r.recognize_google(audio, language=config.voiceTypingLanguage)
         except sr.UnknownValueError:
-            #return "[Speech unrecognized!]"
+            # return "[Speech unrecognized!]"
             return ""
         except sr.RequestError as e:
             return "[Error: {0}]".format(e)
-    elif config.voiceTypingPlatform == "googlecloud" and os.environ["GOOGLE_APPLICATION_CREDENTIALS"] and "Speech-to-Text" in config.enabledGoogleAPIs:
+    elif (
+        config.voiceTypingPlatform == "googlecloud"
+        and os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+        and "Speech-to-Text" in config.enabledGoogleAPIs
+    ):
         # recognize speech using Google Cloud Speech
         try:
             # check availabl languages at: https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages
             # config.voiceTypingLanguage should be code list in column BCP-47 at https://cloud.google.com/speech-to-text/docs/speech-to-text-supported-languages
-            return r.recognize_google_cloud(audio, language=config.voiceTypingLanguage, credentials_json=config.google_cloud_credentials)
+            return r.recognize_google_cloud(
+                audio,
+                language=config.voiceTypingLanguage,
+                credentials_json=config.google_cloud_credentials,
+            )
         except sr.UnknownValueError:
-            #return "[Speech unrecognized!]"
+            # return "[Speech unrecognized!]"
             return ""
         except sr.RequestError as e:
             return "[Error: {0}]".format(e)
@@ -101,29 +122,42 @@ def voiceTyping():
         try:
             # check availabl languages at: https://github.com/openai/whisper/blob/main/whisper/tokenizer.py
             # config.voiceTypingLanguage should be uncapitalized full language name like "english" or "chinese"
-            return r.recognize_whisper(audio, model=config.voiceTypingWhisperEnglishModel if config.voiceTypingLanguage == "english" else "large", language=config.voiceTypingLanguage)
+            return r.recognize_whisper(
+                audio,
+                model=(
+                    config.voiceTypingWhisperEnglishModel
+                    if config.voiceTypingLanguage == "english"
+                    else "large"
+                ),
+                language=config.voiceTypingLanguage,
+            )
         except sr.UnknownValueError:
             return ""
         except sr.RequestError as e:
             return "[Error]"
     elif config.voiceTypingPlatform == "whispercpp":
-        #from speech_recognition.audio import AudioData
-        #assert isinstance(audio, AudioData), "Data must be audio data"
+        # from speech_recognition.audio import AudioData
+        # assert isinstance(audio, AudioData), "Data must be audio data"
         wav_bytes_data = audio.get_wav_data(
             convert_rate=16000,  # audio samples must be 8kHz or 16 kHz
-            convert_width=2  # audio samples should be 16-bit
+            convert_width=2,  # audio samples should be 16-bit
         )
         wav_file = os.path.join(config.packageFolder, "temp", "voice.wav")
         with open(wav_file, "wb") as fileObj:
             fileObj.write(wav_bytes_data)
         # Example of cli: ./main -np -nt -l auto -t 12 -m ggml-large-v3-q5_0.bin -f ~/Desktop/voice.wav
         # *.bin model files available at: https://huggingface.co/ggerganov/whisper.cpp/tree/main
-        if not os.path.isfile(config.whispercpp_main) or not os.path.isfile(config.whispercpp_model):
+        if not os.path.isfile(config.whispercpp_main) or not os.path.isfile(
+            config.whispercpp_model
+        ):
             return "[Error]"
-        cli = f'''"{config.whispercpp_main}" -np -nt -l {'en' if config.voiceTypingLanguage.lower() in ('english', 'en') else 'auto'} -t {getCpuThreads()} -m "{config.whispercpp_model}" -f "{wav_file}" {config.whispercpp_additional_options}'''
-        process = subprocess.Popen(cli.rstrip(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        cli = f""""{config.whispercpp_main}" -np -nt -l {'en' if config.voiceTypingLanguage.lower() in ('english', 'en') else 'auto'} -t {getCpuThreads()} -m "{config.whispercpp_model}" -f "{wav_file}" {config.whispercpp_additional_options}"""
+        process = subprocess.Popen(
+            cli.rstrip(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
         stdout, stderr = process.communicate()
         return "[Error]" if stderr and not stdout else stdout.decode("utf-8").strip()
+
 
 def getCpuThreads():
     if config.cpu_threads and isinstance(config.cpu_threads, int):
@@ -131,16 +165,21 @@ def getCpuThreads():
     physical_cpu_core = psutil.cpu_count(logical=False)
     return physical_cpu_core if physical_cpu_core and physical_cpu_core > 1 else 1
 
+
 def getStringWidth(text):
     width = 0
     for character in text:
         width += wcwidth.wcwidth(character)
     return width
 
+
 def wrapText(content, terminal_width=None):
     if terminal_width is None:
         terminal_width = shutil.get_terminal_size().columns
-    return "\n".join([textwrap.fill(line, width=terminal_width) for line in content.split("\n")])
+    return "\n".join(
+        [textwrap.fill(line, width=terminal_width) for line in content.split("\n")]
+    )
+
 
 def print1(content):
     if config.wrapWords:
@@ -150,34 +189,48 @@ def print1(content):
     else:
         print(content)
 
+
 def print2(content):
-    print_formatted_text(HTML(f"<{config.terminalPromptIndicatorColor2}>{content}</{config.terminalPromptIndicatorColor2}>"))
+    print_formatted_text(
+        HTML(
+            f"<{config.terminalPromptIndicatorColor2}>{content}</{config.terminalPromptIndicatorColor2}>"
+        )
+    )
+
 
 def print3(content):
     splittedContent = content.split(": ", 1)
     if len(splittedContent) == 2:
         key, value = splittedContent
-        print_formatted_text(HTML(f"<{config.terminalPromptIndicatorColor2}>{key}:</{config.terminalPromptIndicatorColor2}> {value}"))
+        print_formatted_text(
+            HTML(
+                f"<{config.terminalPromptIndicatorColor2}>{key}:</{config.terminalPromptIndicatorColor2}> {value}"
+            )
+        )
     else:
         print2(splittedContent)
+
 
 def getHideOutputSuffix():
     return f" > {'nul' if config.thisPlatform == 'Windows' else '/dev/null'} 2>&1"
 
+
 def downloadFile(url, localpath, timeout=60):
     response = requests.get(url, timeout=timeout)
-    with open(localpath, 'wb') as fileObj:
+    with open(localpath, "wb") as fileObj:
         fileObj.write(response.content)
+
 
 def isCommandInstalled(package):
     return True if shutil.which(package.split(" ", 1)[0]) else False
 
+
 def installPipPackage(module, update=True):
-    #executablePath = os.path.dirname(sys.executable)
-    #pippath = os.path.join(executablePath, "pip")
-    #pip = pippath if os.path.isfile(pippath) else "pip"
-    #pip3path = os.path.join(executablePath, "pip3")
-    #pip3 = pip3path if os.path.isfile(pip3path) else "pip3"
+    # executablePath = os.path.dirname(sys.executable)
+    # pippath = os.path.join(executablePath, "pip")
+    # pip = pippath if os.path.isfile(pippath) else "pip"
+    # pip3path = os.path.join(executablePath, "pip3")
+    # pip3 = pip3path if os.path.isfile(pip3path) else "pip3"
 
     if isCommandInstalled("pip"):
         pipInstallCommand = f"{sys.executable} -m pip install"
@@ -187,7 +240,12 @@ def installPipPackage(module, update=True):
                 pipFailedUpdated = "pip tool failed to be updated!"
                 try:
                     # Update pip tool in case it is too old
-                    updatePip = subprocess.Popen(f"{pipInstallCommand} --upgrade pip", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    updatePip = subprocess.Popen(
+                        f"{pipInstallCommand} --upgrade pip",
+                        shell=True,
+                        stdout=subprocess.PIPE,
+                        stderr=subprocess.PIPE,
+                    )
                     *_, stderr = updatePip.communicate()
                     if not stderr:
                         print("pip tool updated!")
@@ -197,18 +255,27 @@ def installPipPackage(module, update=True):
                     print(pipFailedUpdated)
                 config.isPipUpdated = True
         try:
-            upgrade = (module.startswith("-U ") or module.startswith("--upgrade "))
+            upgrade = module.startswith("-U ") or module.startswith("--upgrade ")
             if upgrade:
                 moduleName = re.sub("^[^ ]+? (.+?)$", r"\1", module)
             else:
                 moduleName = module
             print(f"{'Upgrading' if upgrade else 'Installing'} '{moduleName}' ...")
-            installNewModule = subprocess.Popen(f"{pipInstallCommand} {module}", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            installNewModule = subprocess.Popen(
+                f"{pipInstallCommand} {module}",
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
             *_, stderr = installNewModule.communicate()
             if not stderr:
-                print(f"Package '{moduleName}' {'upgraded' if upgrade else 'installed'}!")
+                print(
+                    f"Package '{moduleName}' {'upgraded' if upgrade else 'installed'}!"
+                )
             else:
-                print(f"Failed {'upgrading' if upgrade else 'installing'} package '{moduleName}'!")
+                print(
+                    f"Failed {'upgrading' if upgrade else 'installing'} package '{moduleName}'!"
+                )
                 if config.developer:
                     print(stderr)
             return True
@@ -219,19 +286,20 @@ def installPipPackage(module, update=True):
         print("pip command is not found!")
         return False
 
+
 def saveConfig():
     temporaryConfigs = [
-        'new_chat_response', 
-        'localStorage', 
-        'packageFolder', 
-        'tempChunk', 
-        'selectAll', 
-        'pagerContent', 
-        'tempChunk', 
-        'ttsPlatform', 
-        'isPipUpdated',
-        'thisPlatform',
-        'pyaudioInstalled',
+        "new_chat_response",
+        "localStorage",
+        "packageFolder",
+        "tempChunk",
+        "selectAll",
+        "pagerContent",
+        "tempChunk",
+        "ttsPlatform",
+        "isPipUpdated",
+        "thisPlatform",
+        "pyaudioInstalled",
     ]
     excludeConfigList = []
     configFile = os.path.join(config.packageFolder, "config.py")
@@ -246,11 +314,12 @@ def saveConfig():
                 except:
                     pass
 
+
 def getGroqApi_key():
-    '''
+    """
     support multiple grop api keys to work around rate limit
     User can manually edit config to change the value of config.groqApi_key to a list of multiple api keys instead of a string of a single api key
-    '''
+    """
     if config.groqApi_key:
         if isinstance(config.groqApi_key, str):
             return config.groqApi_key
@@ -263,6 +332,7 @@ def getGroqApi_key():
             return ""
     else:
         return ""
+
 
 def getGroqClient():
     return Groq(api_key=getGroqApi_key())
--- package/groqchat/utils/tts_utils.py
+++ package/groqchat/utils/tts_utils.py
@@ -1,5 +1,11 @@
-from groqchat import config, downloadFile, installPipPackage, getHideOutputSuffix, print2
-import os, traceback, sounddevice, soundfile, pydoc, shutil
+from groqchat import (
+    config,
+    downloadFile,
+    installPipPackage,
+    getHideOutputSuffix,
+    print2,
+)
+import os, traceback, pydoc, shutil
 
 
 class TTSUtil:
@@ -11,27 +17,50 @@ class TTSUtil:
                 if shutil.which("termux-tts-speak"):
                     # Use termux-tts-speak via termux API on Android
                     # read https://wiki.termux.com/wiki/Termux-tts-speak
-                    additional_options = f" {config.termuxtts_additional_options.strip()}" if config.termuxtts_additional_options.strip() else ""
-                    voice = f" -l {config.termuxtts_voice.strip()}" if config.termuxtts_voice.strip() else ""
+                    additional_options = (
+                        f" {config.termuxtts_additional_options.strip()}"
+                        if config.termuxtts_additional_options.strip()
+                        else ""
+                    )
+                    voice = (
+                        f" -l {config.termuxtts_voice.strip()}"
+                        if config.termuxtts_voice.strip()
+                        else ""
+                    )
                     cmd = f"termux-tts-speak -r {config.vlcSpeed}{voice}{additional_options}"
                     pydoc.pipepager(content, cmd=cmd)
                 elif config.thisPlatform == "macOS":
-                    additional_options = f" {config.say_additional_options.strip()}" if config.say_additional_options.strip() else ""
-                    voice = f" -v {config.say_voice.strip()}" if config.say_voice.strip() else ""
+                    additional_options = (
+                        f" {config.say_additional_options.strip()}"
+                        if config.say_additional_options.strip()
+                        else ""
+                    )
+                    voice = (
+                        f" -v {config.say_voice.strip()}"
+                        if config.say_voice.strip()
+                        else ""
+                    )
                     cmd = f"say -r {config.say_speed}{voice}{additional_options}"
                     pydoc.pipepager(content, cmd=cmd)
                 elif config.thisPlatform == "Windows":
 
                     homeWsay = os.path.join(config.localStorage, "gchat", "wsay.exe")
-                    isWsayFound = (shutil.which("wsay") or os.path.isfile(homeWsay))
+                    isWsayFound = shutil.which("wsay") or os.path.isfile(homeWsay)
                     if not isWsayFound:
                         print2("Downloading 'wsay.exe' ...")
-                        downloadFile("https://github.com/p-groarke/wsay/releases/download/1.6.2/wsay.exe", homeWsay)
-                        isWsayFound = (shutil.which("wsay") or os.path.isfile(homeWsay))
+                        downloadFile(
+                            "https://github.com/p-groarke/wsay/releases/download/1.6.2/wsay.exe",
+                            homeWsay,
+                        )
+                        isWsayFound = shutil.which("wsay") or os.path.isfile(homeWsay)
                         print2(f"Saved in: {homeWsay}")
 
-                    additional_options = f" {config.wsay_additional_options.strip()}" if config.wsay_additional_options.strip() else ""
-                    cmd = f'''"{homeWsay if os.path.isfile(homeWsay) else 'wsay'}" --voice {config.wsay_voice} --speed {config.wsay_speed}{additional_options}'''
+                    additional_options = (
+                        f" {config.wsay_additional_options.strip()}"
+                        if config.wsay_additional_options.strip()
+                        else ""
+                    )
+                    cmd = f""""{homeWsay if os.path.isfile(homeWsay) else 'wsay'}" --voice {config.wsay_voice} --speed {config.wsay_speed}{additional_options}"""
                     pydoc.pipepager(content, cmd=cmd)
                 elif config.thisPlatform == "Linux" and not config.isTermux:
                     if not shutil.which("piper"):
@@ -39,25 +68,33 @@ class TTSUtil:
                         if not install:
                             return None
                     audioFile = os.path.join(config.packageFolder, "temp", "piper.wav")
-                    model_dir = os.path.join(config.localStorage, "gchat", "LLMs", "piper")
-                    model_path = f"""{os.path.join(model_dir, config.piper_voice)}.onnx"""
+                    model_dir = os.path.join(
+                        config.localStorage, "gchat", "LLMs", "piper"
+                    )
+                    model_path = (
+                        f"""{os.path.join(model_dir, config.piper_voice)}.onnx"""
+                    )
                     model_config_path = f"""{model_path}.json"""
-                    piper_additional_options = f" {config.piper_additional_options.strip()}" if config.piper_additional_options.strip() else ""
+                    piper_additional_options = (
+                        f" {config.piper_additional_options.strip()}"
+                        if config.piper_additional_options.strip()
+                        else ""
+                    )
                     if os.path.isfile(model_path):
                         if shutil.which("cvlc"):
-                            cmd = f'''"{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output-raw | cvlc --play-and-exit --rate {config.vlcSpeed} --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=22050{piper_additional_options} -{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output-raw | cvlc --play-and-exit --rate {config.vlcSpeed} --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=22050{piper_additional_options} -{getHideOutputSuffix()}"""
                         elif shutil.which("aplay"):
-                            cmd = f'''"{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output-raw | aplay -r 22050 -f S16_LE -t raw{piper_additional_options} -{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output-raw | aplay -r 22050 -f S16_LE -t raw{piper_additional_options} -{getHideOutputSuffix()}"""
                         else:
-                            cmd = f'''"{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output_file "{audioFile}"{piper_additional_options}{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model "{model_path}" --config "{model_config_path}" --output_file "{audioFile}"{piper_additional_options}{getHideOutputSuffix()}"""
                     else:
                         print2("[Downloading voice ...] ")
                         if shutil.which("cvlc"):
-                            cmd = f'''"{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output-raw | cvlc --play-and-exit --rate {config.vlcSpeed} --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=22050{piper_additional_options} -{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output-raw | cvlc --play-and-exit --rate {config.vlcSpeed} --demux=rawaud --rawaud-channels=1 --rawaud-samplerate=22050{piper_additional_options} -{getHideOutputSuffix()}"""
                         elif shutil.which("aplay"):
-                            cmd = f'''"{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output-raw | aplay -r 22050 -f S16_LE -t raw{piper_additional_options} -{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output-raw | aplay -r 22050 -f S16_LE -t raw{piper_additional_options} -{getHideOutputSuffix()}"""
                         else:
-                            cmd = f'''"{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output_file "{audioFile}"{piper_additional_options}{getHideOutputSuffix()}'''
+                            cmd = f""""{shutil.which("piper")}" --model {config.piper_voice} --download-dir "{model_dir}" --data-dir "{model_dir}" --output_file "{audioFile}"{piper_additional_options}{getHideOutputSuffix()}"""
                     pydoc.pipepager(content, cmd=cmd)
                     if not shutil.which("cvlc") and not shutil.which("aplay"):
                         TTSUtil.playAudioFile(audioFile)
@@ -69,5 +106,7 @@ class TTSUtil:
 
     @staticmethod
     def playAudioFile(audioFile):
-        sounddevice.play(*soundfile.read(audioFile)) 
+        import sounddevice, soundfile
+
+        sounddevice.play(*soundfile.read(audioFile))
         sounddevice.wait()
